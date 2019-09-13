0ea2ba5808aea8c9c2a54266be69dd8815cecaae
amandaalmeida89 <amandaalmeida89@gmail.com>
2019-09-12 23:51:55 -0300

    retornando as funcoes de filtro

--- src/main.js
+++ src/main.js
@@ -1,44 +1,41 @@
-const screenTable = document.getElementById("table");
+const yearSelect = document.getElementById("year");
+const transportSelect = document.getElementById("transport");
 
-const createTableRow = arr => {
-  const tr = document.createElement("tr");
-
-  arr.forEach((val) => {
-    const td = document.createElement("td");
-    td.innerText = val;
-    tr.appendChild(td);
-  })
-
-  screenTable.appendChild(tr);
+//cria os options dentro do primeiro seletor
+const createYearsOption = year => {
+  const newOption = document.createElement("option");
+  newOption.innerText = year;
+  newOption.value = year;
+  yearSelect.appendChild(newOption);
+};
+//Popular os anos dentro do primeiro select
+const fillYears = () => {
+  const years = window.filterData.yearInjuries();
+  years.forEach(createYearsOption);
 };
 
-const allYers = () => {
-  const years = window.filterData.yearInjuries();
-  years.unshift("People injured by transportation");
-  createTableRow(years);
+//cria os opstions dentro do segundo seletor
+const createTransportOption = transport => {
+  const newOption = document.createElement("option");
+  newOption.innerText = transport;
+  newOption.value = transport;
+  transportSelect.appendChild(newOption);
+};
+//Popula os options dentro do segundo select
+const fillTransport = () => {
+  const transports = window.filterData.getInjuries();
+  transports.forEach(createTransportOption);
 }
+//Escuta as funções no evento load
+window.addEventListener("load", fillTransport);
+window.addEventListener("load", fillYears);
 
-
-const allValues = () => {
-  const values = window.filterData.valueInjuries();
-  const allInjuries = window.filterData.getInjuries();
-  const cols = [];
-  
-  for(const i in values) {
-    for(const j in values[i]) {
-
-      if(!cols[j]) {
-        cols[j] = [ allInjuries[j] ];
-      }
-
-      cols[j].push( values[i][j] );
-
-    }
-  }
-
-  cols.forEach(createTableRow);
-}
-
-window.addEventListener("load", allYers);
-window.addEventListener("load", allValues);
+//Escuta as funções no click
+document.getElementById("btn").addEventListener("click", () => {
+  const yearSelected = yearSelect.value;
+  const transportSelected = transportSelect.value;
+  const result = window.filterData.filterByYear(yearSelected);
+  document.getElementById("result").innerText = "This mode of transport had in the selected year " +  result[transportSelected] + " people injured.";
+  event.preventDefault();
+})
 
