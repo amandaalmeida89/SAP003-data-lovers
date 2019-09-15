8c3b7439d6e4e5fdf58c6148df42f304e863a477
amandaalmeida89 <amandaalmeida89@gmail.com>
2019-09-15 09:30:59 -0300

    Alteração da Interface

--- src/data.js
+++ src/data.js
@@ -1,23 +1,19 @@
-//informando que é data e pega o ANO
-const yearInjuries = () =>
+const accidentGrouping = (filteredYear) =>
   INJURIES
-    .map(inj => new Date(inj.Year))
-    .map(date => date.getFullYear());
-
-    //informa os nomes dos transportes
-const getInjuries = () =>
-  Object.keys(INJURIES[0]).filter(item => item != "Year");
-
-const filterByYears = years =>
-  INJURIES.filter(item => {
-    const date = new Date(item.Year);
-    return years.includes(date.getFullYear().toString());
-  })
+  .filter(item => filteredYear ? (new Date(item.Year).getFullYear()) == filteredYear : true )
+  .map(item => {
+    return {
+      year: new Date(item.Year).getFullYear(),
+      airplane: item.Total_Injured_Persons_General_Aviation + item.Total_Injured_Persons_On_Demand_Air_Taxi + item.Total_Injured_Persons_US_Air_Carrier,
+      boat: item.Total_Injured_Persons_Freight_Vessel + item.Total_Injured_Persons_Passenger_Vessel + item.Total_Injured_Persons_Recreational_Boating,
+      auto: item.Total_Injured_Persons_Bus_Occupants + item.Total_Injured_Persons_Commuter_Carrier + item.Total_Injured_Persons_Employee_Or_Worker + item.Total_Injured_Persons_Passenger_Car_Occupants + item.Total_Injured_Persons_Truck_Occupants_Large + item.Total_Injured_Persons_Truck_Occupants_Light,
+      motorcycle: item.Total_Injured_Persons_Motorcyclists,
+      bicycle: item.Total_Injured_Persons_Pedalcyclists,
+    }
+  });
 
 window.filterData = {
-  yearInjuries: yearInjuries,
-  getInjuries: getInjuries,
-  filterByYears: filterByYears,
+  accidentGrouping: accidentGrouping,
 };
 
 window.sortData = {
