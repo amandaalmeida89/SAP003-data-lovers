8aa36d92642812e423f7e0b6b3a112b216a1fcc6
camilasuehiro <camilasuehiro@yahoo.com.br>
2019-09-20 15:57:21 -0300

    sort

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export const filterData = (data, filteredFirstYear, filteredSecondYear) =>
   data.map(item => {
     return {
-      year: new Date(item.Year).getFullYear(),
+      year: new Date(item.Year).getFullYear(), 
       airplane: item.Total_Injured_Persons_Commuter_Carrier + item.Total_Injured_Persons_General_Aviation + item.Total_Injured_Persons_On_Demand_Air_Taxi + item.Total_Injured_Persons_US_Air_Carrier,
       boat: item.Total_Injured_Persons_Freight_Vessel + item.Total_Injured_Persons_Passenger_Vessel + item.Total_Injured_Persons_Recreational_Boating,
       auto: item.Total_Injured_Persons_Passenger_Or_Occupant + item.Total_Injured_Persons_Bus_Occupants + item.Total_Injured_Persons_Employee_Or_Worker + item.Total_Injured_Persons_Passenger_Car_Occupants + item.Total_Injured_Persons_Truck_Occupants_Large + item.Total_Injured_Persons_Truck_Occupants_Light + item.Total_Injured_Persons_Pedestrians,
@@ -35,8 +35,7 @@ export const filterData = (data, filteredFirstYear, filteredSecondYear) =>
 
 window.filterData = filterData;
 
-window.sortData = {
-};
+window.sortData = sortData;
 
 window.computeStats = {
 };
